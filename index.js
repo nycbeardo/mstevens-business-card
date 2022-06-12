5c147d8b7fa8d2665f26028fa2bbf43e5c3d0f64
Montique Stevens <mstevens528@gmail.com>
2022-06-11 21:16:39 -0400

    new commit

---
+++ index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log("Hey I'm Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users. \n\nYou can find me online via these links: \n\nTwitter: https://twitter.com/NYCBEARDO \n\nMy Site: https://mkstevens.dev/ \n\nGithub: https://github.com/nycbeardo \n\nPolywork: https://www.polywork.com/mstevens ")
