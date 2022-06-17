541be43f053a8c9e838a251373ce891e8f53b450
Montique Stevens <mstevens528@gmail.com>
2022-06-17 17:41:08 -0400

    correction of spelling errors

--- index.mjs
+++ index.mjs
@@ -5,8 +5,8 @@ import boxen from "boxen";
 const boxenSpecs = {
     padding: 1,
     margin: 1,
-    borderStyle: double,
+    borderStyle: "double",
     title: "nycbeardo",
 };
 
-console.log(boxen('Hey Im Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users. \n\nYou can find me online via these links: \n\nTwitter: https://twitter.com/NYCBEARDO \n\nMy Site: https://mkstevens.dev/ \n\nGithub: https://github.com/nycbeardo \n\nPolywork: https://www.polywork.com/mstevens'));
+console.log(boxen('Hey Im Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users.\n\nYou can find me online via these links:\n\nTwitter: https://twitter.com/NYCBEARDO\n\nMy Site: https://mkstevens.dev/\n\nGithub: https://github.com/nycbeardo\n\nPolywork: https://www.polywork.com/mstevens'));
