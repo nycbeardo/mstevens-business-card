23376d1d853652d8ced05e47bec9aaddad79b22e
Montique Stevens <mstevens528@gmail.com>
2022-06-17 18:02:35 -0400

    spelling errors

--- index.mjs
+++ index.mjs
@@ -9,4 +9,5 @@ const boxenSpecs = {
     title: "nycbeardo",
 };
 
-console.log(boxen('Hey Im Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users.\n\nYou can find me online via these links:\n\nTwitter: https://twitter.com/NYCBEARDO\n\nMy Site: https://mkstevens.dev/\n\nGithub: https://github.com/nycbeardo\n\nPolywork: https://www.polywork.com/mstevens'));
+const intro = ("Hey Im Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users.\n\nYou can find me online via these links:\n\nTwitter: https://twitter.com/NYCBEARDO\n\nMy Site: https://mkstevens.dev/\n\nGithub: https://github.com/nycbeardo\n\nPolywork: https://www.polywork.com/mstevens");
+console.log(boxen(intro + boxenSpecs));
