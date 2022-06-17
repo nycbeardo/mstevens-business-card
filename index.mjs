bc6f1407640e916721cb76425d8a79161fccaad3
Montique Stevens <mstevens528@gmail.com>
2022-06-17 17:21:42 -0400

    punctuation changes

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const boxenSpecs = {
     padding: 1,
     margin: 1,
     borderStyle: double,
-    title: "nycbeardo"
-}
+    title: "nycbeardo",
+};
 
 console.log(boxen('Hey Im Monty! Im a dev that loves everything about the web, generative art, interaction design, and how it relates to all users. \n\nYou can find me online via these links: \n\nTwitter: https://twitter.com/NYCBEARDO \n\nMy Site: https://mkstevens.dev/ \n\nGithub: https://github.com/nycbeardo \n\nPolywork: https://www.polywork.com/mstevens'));
